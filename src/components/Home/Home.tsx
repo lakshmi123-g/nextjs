f8226deeff495d6e58900c0baaf4aca70d734e42
durga <durga.gangisetty@engro.io>
2025-03-06 15:49:23 +0530

    implemented popular rail

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ZHero from './Hero/ZHero'
+import Destination from './Destination/Destination'
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
       <ZHero/>
+      <Destination/>
        </div>
   )
 }
